445c7597127bf8e06a255133aef60ccb35b877a1
yvettepinder <yvette.pinder.16@ucl.ac.uk>
2017-12-11 01:30:55 +0000

    Create a test for multiple commands

--- src/Tests/PositionTests.java
+++ src/Tests/PositionTests.java
@@ -15,10 +15,19 @@ public class PositionTests {
         rover = new Rover();
     }
 
+    // test to check initial position with one command
     @Test
-    public void testPosition(){
+    public void testPositionSingleCommand(){
         String newPosition = rover.findNewPosition("0,0,N", "F");
         Assert.assertTrue(newPosition.equals("0,1,N"));
+        System.out.print(newPosition);
+    }
+
+    //test to check initial position with multiple commands
+    @Test
+    public void testPositionMultipleCommands(){
+        String newPosition = rover.findNewPosition("0,0,N", "FRFLFRR");
+        Assert.assertTrue(newPosition.equals("1,2,S"));
         System.out.print(newPosition);
     }
 }
